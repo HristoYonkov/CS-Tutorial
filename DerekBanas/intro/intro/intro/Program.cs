bc9ff40f31c18ccdc0e337e327f9a74149052619
Hristo Yonkov <hristopturs@gmail.com>
2025-02-05 18:01:11 +0200

    Create Program.cs

---
+++ DerekBanas/intro/intro/intro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Intro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] customerNames = new string[2, 2] { {"Hristo", "Yonkov"}, { "Pavel", "Dimitrov"} };
+            Console.WriteLine($"MD Value : {customerNames.GetValue(0, 0)}");
+        }
+    }
+}
