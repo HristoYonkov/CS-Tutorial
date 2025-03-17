fb7c485c664b6ca6197ec53ab9a1393b9846e55d
Hristo Yonkov <50380455+HristoYonkov@users.noreply.github.com>
2025-03-17 12:32:27 +0200

    add Dictionary example

--- DerekBanas/Sets and Dictionaries/Program.cs
+++ DerekBanas/Sets and Dictionaries/Program.cs
@@ -6,7 +6,23 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            
+            //var names = new SortedDictionary<string, int>(); // Dictionary with sorted keys.
+            var names = new Dictionary<string, int>();
+            names["Niki"] = 3;
+            names.Add("Stoyan", 2); // names["Stoyan"] = 2;
+            names.Remove("Stoyan");
+            if (names.ContainsKey("Viki")) // In order to check if a key exist we use ContainsKey() method. otherwise if we do not check there will be error with the program.
+            {
+                Console.WriteLine(names["Viki"]);
+            }
+
+            names["Viktor"] = 4;
+            names["Ivo"] = 1;
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+            // We also have ContainsValue() method but it is a very slow operation, so we may want to avoid this if we can.
         }
     }
 }
