33789f24e9aa6c68c600b1d36c166ada569a1dde
Hristo Yonkov <50380455+HristoYonkov@users.noreply.github.com>
2025-03-17 18:12:25 +0200

    update lessons

---
+++ DerekBanas/StreamsFilesAndDirectories/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(@"C:\Temp\output.txt");
+            sw.WriteLine("Hello!!!");
+            sw.Close();
+        }
+    }
+}
