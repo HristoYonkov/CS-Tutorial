9ca0588f7ba930922e5f284e08c3d4b3f578ba17
Hristo Yonkov <50380455+HristoYonkov@users.noreply.github.com>
2025-03-18 12:24:51 +0200

    update the lessons

--- DerekBanas/StreamsFilesAndDirectories/Program.cs
+++ DerekBanas/StreamsFilesAndDirectories/Program.cs
@@ -31,6 +31,17 @@ namespace MyApp
             byte[] read = new byte[2];
             fileStreamRead.Seek(2, SeekOrigin.Begin); // Set pointer at the third position of the array.
             fileStreamRead.Read(read, 0, 2); // Will read "ll".
+
+            //Another usefull methods that we can use and make operations on 1 row of codes are:
+            string fileContent = File.ReadAllText("someFile.txt"); // we need to have this file
+            string[] fileContent2 = File.ReadAllLines("someFile.txt"); // we need to have this file
+            File.WriteAllText(@"C:\Temp\All.txt", "Sentence inside the file."); //Create the file and put the text inside the file.
+
+            var arrOfStrings = new string[] { "Line1", "Line2", "Line3" };
+            File.WriteAllLines(@"C:\Temp\Lines.txt", arrOfStrings); // Create a file with n count of text rows.
+            File.AppendAllText(@"C:\Temp\Lines.txt", "Added new Line."); // Opens the file and add new lines of text.
+
+            //We also can use direcories length of directories but for that need just check the google or lection again.
         }
     }
 }
