2b764786331526bac3d622cbabbf1bf7c8a11d5e
Hristo Yonkov <50380455+HristoYonkov@users.noreply.github.com>
2025-03-18 11:15:27 +0200

    update example

--- DerekBanas/StreamsFilesAndDirectories/Program.cs
+++ DerekBanas/StreamsFilesAndDirectories/Program.cs
@@ -9,6 +9,28 @@ namespace MyApp
             StreamWriter sw = new StreamWriter(@"C:\Temp\output.txt");
             sw.WriteLine("Hello!!!");
             sw.Close();
+
+            StreamReader sr = new StreamReader(@"C:\Temp\countries.txt");
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+            sr.Close();
+
+            //Here we can read and write with byte numbers.
+            FileStream fileStreamRead = new FileStream(@"C:\Temp\output.txt", FileMode.OpenOrCreate);
+            byte[] buffer = new byte[100];
+            fileStreamRead.Read(buffer, 0, 25);
+
+            //Writing values
+            FileStream fileStreamWrite = new FileStream(@"C:\Temp\outputWrite.txt", FileMode.OpenOrCreate);
+            byte[] data = new byte[] { 72, 85, 73 };
+            fileStreamWrite.Write(data, 0, data.Length);
+            fileStreamWrite.Close();
+
+            //ReaderWriterLock exact values
+            byte[] read = new byte[2];
+            fileStreamRead.Seek(2, SeekOrigin.Begin); // Set pointer at the third position of the array.
+            fileStreamRead.Read(read, 0, 2); // Will read "ll"
         }
     }
 }
